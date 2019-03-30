1edf2d6a031d5db75b535b41fe00bbaf5b387e19
dylan.wood <dylan.swimwood@gmail.com>
2019-03-30 16:59:19 -0300

    add morse 2 english functionality

--- app/src/main/java/com/rubinlabs/morsecode/MorseCodeToEnglishActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/MorseCodeToEnglishActivity.java
@@ -1,14 +1,24 @@
 package com.rubinlabs.morsecode;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MorseCodeToEnglishActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +26,69 @@ public class MorseCodeToEnglishActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.morseButton);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // content
+        EditText morse_edittext = findViewById(R.id.morseTextView);
+        TextView english_textview = findViewById(R.id.englishTextView);
+        FloatingActionButton morse_btn = findViewById(R.id.morseButton);
+        Button char_btn = findViewById(R.id.nextCharacterButton);
+        Button word_btn = findViewById(R.id.nextWordButton);
+
+        // translator
+        MorseCodeTranslator mct = MorseCodeTranslator.getInstance();
+
+        morse_edittext.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                v.onTouchEvent(event);
+                InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                return true;
             }
         });
+
+        morse_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String new_text = morse_edittext.getText() + ".";
+                morse_edittext.setText(new_text);
+                morse_edittext.setSelection(new_text.length());
+            }
+        });
+
+        morse_btn.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+                String new_text = morse_edittext.getText() + "-";
+                morse_edittext.setText(new_text);
+                morse_edittext.setSelection(new_text.length());
+                return  true;
+            }
+        });
+
+        char_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String new_text = morse_edittext.getText() + " ";
+                morse_edittext.setText(new_text);
+                morse_edittext.setSelection(new_text.length());
+                english_textview.setText(mct.morseWordToEnglishWord(new_text));
+            }
+        });
+
+        word_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String new_text = morse_edittext.getText() + " / ";
+                morse_edittext.setText(new_text);
+                morse_edittext.setSelection(new_text.length());
+                english_textview.setText(mct.morseWordToEnglishWord(new_text));
+            }
+        });
+
+        morse_edittext.requestFocus();
+
+
     }
 
 }
