e0ffb20ddeaafe7584186a9d63c6296a070f6b80
Dylan <dylan.swimwood@gmail.com>
2019-03-30 20:20:42 -0300

    added reset button and sound files (#12)
    * added reset button and sound files
    
    * fix wav file naming
    
    * fix constraint

--- app/src/main/java/com/rubinlabs/morsecode/MainActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Morse Code Translator");
         setContentView(R.layout.activity_main);
 
         e2m_btn = (Button) findViewById(R.id.English2MorseButton);
--- app/src/main/java/com/rubinlabs/morsecode/MorseCodeToEnglishActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/MorseCodeToEnglishActivity.java
@@ -6,26 +6,32 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MorseCodeToEnglishActivity extends AppCompatActivity {
+    private EditText m2eInput;
+    private TextView m2eOutput;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Morse Code to English");
         setContentView(R.layout.activity_morse_code_to_english);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // content
-        EditText morse_edittext = findViewById(R.id.m2eInputTextView);
-        TextView english_textview = findViewById(R.id.m2eOutputTextView);
+        m2eInput = findViewById(R.id.m2eInputTextView);
+        m2eOutput = findViewById(R.id.m2eOutputTextView);
         FloatingActionButton morse_btn = findViewById(R.id.morseButton);
         Button char_btn = findViewById(R.id.nextCharacterButton);
         Button word_btn = findViewById(R.id.nextWordButton);
@@ -33,7 +39,7 @@ public class MorseCodeToEnglishActivity extends AppCompatActivity {
         // translator
         MorseCodeTranslator mct = MorseCodeTranslator.getInstance();
 
-        morse_edittext.setOnTouchListener(new View.OnTouchListener() {
+        m2eInput.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 v.onTouchEvent(event);
@@ -48,17 +54,17 @@ public class MorseCodeToEnglishActivity extends AppCompatActivity {
         morse_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String new_text = morse_edittext.getText() + ".";
-                morse_edittext.setText(new_text);
-                morse_edittext.setSelection(new_text.length());
+                String new_text = m2eInput.getText() + ".";
+                m2eInput.setText(new_text);
+                m2eInput.setSelection(new_text.length());
             }
         });
 
         morse_btn.setOnLongClickListener(new View.OnLongClickListener() {
             public boolean onLongClick(View v) {
-                String new_text = morse_edittext.getText() + "-";
-                morse_edittext.setText(new_text);
-                morse_edittext.setSelection(new_text.length());
+                String new_text = m2eInput.getText() + "-";
+                m2eInput.setText(new_text);
+                m2eInput.setSelection(new_text.length());
                 return  true;
             }
         });
@@ -66,26 +72,47 @@ public class MorseCodeToEnglishActivity extends AppCompatActivity {
         char_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String new_text = morse_edittext.getText() + " ";
-                morse_edittext.setText(new_text);
-                morse_edittext.setSelection(new_text.length());
-                english_textview.setText(mct.morseWordToEnglishWord(new_text));
+                String new_text = m2eInput.getText() + " ";
+                m2eInput.setText(new_text);
+                m2eInput.setSelection(new_text.length());
+                m2eOutput.setText(mct.morseWordToEnglishWord(new_text));
             }
         });
 
         word_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String new_text = morse_edittext.getText() + " / ";
-                morse_edittext.setText(new_text);
-                morse_edittext.setSelection(new_text.length());
-                english_textview.setText(mct.morseWordToEnglishWord(new_text));
+                String new_text = m2eInput.getText() + " / ";
+                m2eInput.setText(new_text);
+                m2eInput.setSelection(new_text.length());
+                m2eOutput.setText(mct.morseWordToEnglishWord(new_text));
             }
         });
 
-        morse_edittext.requestFocus();
+        m2eInput.requestFocus();
 
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.reset_button) {
+            m2eInput.setText(null);
+            m2eInput.setSelection(0);
+            m2eOutput.setText(null);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
