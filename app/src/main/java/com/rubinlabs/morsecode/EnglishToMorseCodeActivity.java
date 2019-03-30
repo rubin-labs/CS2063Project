0bb559fc603c23650d0f0f642c308c9ded3aeb9d
dylan.wood <dylan.swimwood@gmail.com>
2019-03-30 19:06:09 -0300

    added reset button and sound files

--- app/src/main/java/com/rubinlabs/morsecode/EnglishToMorseCodeActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/EnglishToMorseCodeActivity.java
@@ -5,22 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class EnglishToMorseCodeActivity extends AppCompatActivity {
+    EditText e2mInput;
+    TextView e2mOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("English to Morse Code");
         setContentView(R.layout.activity_english_to_morse_code);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Content of Activity
-        EditText e2mInput = findViewById(R.id.e2mInputTextView);
-        TextView e2mOutput = findViewById(R.id.e2mOutputTextView);
+        e2mInput = findViewById(R.id.e2mInputTextView);
+        e2mOutput = findViewById(R.id.e2mOutputTextView);
         FloatingActionButton translateButton = findViewById(R.id.translateButton);
 
         // Create Morse Code Translator Engine
@@ -35,6 +40,28 @@ public class EnglishToMorseCodeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.reset_button) {
+            e2mInput.setText(null);
+            e2mInput.setSelection(0);
+            e2mOutput.setText(null);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
