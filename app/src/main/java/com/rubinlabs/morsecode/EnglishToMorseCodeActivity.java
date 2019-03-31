de6c07940161ac0cd7e8f01d72bff854a63dc85b
Dylan <dylan.swimwood@gmail.com>
2019-03-31 14:30:30 -0300

    added beep and flash for english to morse code (#15)
    * added beep and flash for english to morse code
    
    * Removed debug statements and added callback to release the mediaplayer

--- app/src/main/java/com/rubinlabs/morsecode/EnglishToMorseCodeActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/EnglishToMorseCodeActivity.java
@@ -1,6 +1,13 @@
 package com.rubinlabs.morsecode;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,7 +22,11 @@ import java.util.ArrayList;
 public class EnglishToMorseCodeActivity extends AppCompatActivity {
     private EditText e2mInput;
     private TextView e2mOutput;
-    private MorseCodePlayer morseCodePlayer;
+    private MediaPlayer myMediaPlayer;
+    private CameraManager myCameraManager;
+    private String myCameraID;
+    private Thread myThread;
+    private int time_unit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +44,17 @@ public class EnglishToMorseCodeActivity extends AppCompatActivity {
         // Create Morse Code Translator Engine
         MorseCodeTranslator myTranslator = MorseCodeTranslator.getInstance();
 
-        // Create MorseCodePlayer
-        morseCodePlayer = new MorseCodePlayer(this);
+        // Create Media Player
+        myMediaPlayer = MediaPlayer.create(this, R.raw.dah);
+        time_unit = myMediaPlayer.getDuration() / 3;
+
+        // Create a Camera Manager object
+        try {
+            myCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            myCameraID = myCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
         translateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,10 +97,55 @@ public class EnglishToMorseCodeActivity extends AppCompatActivity {
                     }
                 }
 
-                for (int i=0; i<mediaInstructions.size(); i++) {
-                    myInput = myInput + mediaInstructions.get(i);
-                }
-                e2mOutput.setText(myInput);
+                myThread = new Thread() {
+                    Boolean isRunning = true;
+
+                    @Override
+                    public void run() {
+                        int i = 0;
+                        try {
+                            while (i++ < mediaInstructions.size() && isRunning) {
+                                switch (mediaInstructions.get(i)) {
+
+                                    case 0:
+                                        myCameraManager.setTorchMode(myCameraID, true);
+                                        myMediaPlayer.start();
+                                        Thread.sleep(time_unit);
+                                        break;
+                                    case 1:
+                                        myCameraManager.setTorchMode(myCameraID, true);
+                                        myMediaPlayer.start();
+                                        Thread.sleep(3 * time_unit);
+                                        break;
+                                    case 2:
+                                        Thread.sleep(time_unit);
+                                        break;
+                                    case 3:
+                                        Thread.sleep(3 * time_unit);
+                                        break;
+                                    case 4:
+                                        Thread.sleep(7 * time_unit);
+                                        break;
+                                }
+                                myCameraManager.setTorchMode(myCameraID, false);
+                                if (myMediaPlayer.isPlaying()) {
+                                    myMediaPlayer.pause();
+                                    myMediaPlayer.seekTo(0);
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    }
+
+                    @Override
+                    public void interrupt() {
+                        super.interrupt();
+                        isRunning = false;
+                    }
+                };
+
+                myThread.start();
             }
         });
     }
@@ -107,4 +172,13 @@ public class EnglishToMorseCodeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        myMediaPlayer.release();
+        myThread.interrupt();
+
+        // Turn off flash if it is still on
+        try { myCameraManager.setTorchMode(myCameraID, false); } catch (Exception e) { System.out.println(e); }
+    }
 }
