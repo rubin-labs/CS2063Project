9d10c03573b31b4353e0c608aa180b8e430d8b34
Dylan <dylan.swimwood@gmail.com>
2019-04-01 19:58:13 -0300

    Added navigation and settings page (#16)

--- app/src/main/java/com/rubinlabs/morsecode/MainActivity.java
+++ app/src/main/java/com/rubinlabs/morsecode/MainActivity.java
@@ -2,39 +2,13 @@ package com.rubinlabs.morsecode;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-                default:
-                    break;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Morse Code Translator");
         setContentView(R.layout.activity_main);
 
-        Button e2m_btn = findViewById(R.id.English2MorseButton);
-        Button m2e_btn = findViewById(R.id.Morse2EnglishButton);
-
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FloatingActionButton e2m_btn = findViewById(R.id.English2MorseButton);
+        FloatingActionButton m2e_btn = findViewById(R.id.Morse2EnglishButton);
+        FloatingActionButton settings_btn = findViewById(R.id.SettingsButton);
 
         e2m_btn.setOnClickListener(v -> startActivityForResult(new Intent(MainActivity.this, EnglishToMorseCodeActivity.class),1));
-
         m2e_btn.setOnClickListener(v -> startActivityForResult(new Intent(MainActivity.this, MorseCodeToEnglishActivity.class),1));
+        settings_btn.setOnClickListener(v -> startActivityForResult(new Intent(MainActivity.this, SettingsActivity.class),1));
     }
-
 }
